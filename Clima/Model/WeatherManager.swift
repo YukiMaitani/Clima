e2869ecd88a641bec6b8a543328376f3f0fc6dc0
YukiMaitani <fightzoi1997@gmail.com>
2022-06-16 23:25:08 +0900

    天気の状態の取得

--- Clima/Model/WeatherManager.swift
+++ Clima/Model/WeatherManager.swift
@@ -37,9 +37,32 @@ struct WeatherManager {
         let decorder = JSONDecoder()
         do{
             let decodedData = try decorder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.weather[0].description)
+            let id = decodedData.weather[0].id
+            print(getConditionName(weatherID: id))
         } catch {
             print(error)
         }
+    }
+    
+    func getConditionName(weatherID: Int) -> String {
+        switch weatherID {
+        case 200...232:
+            return "cloud.bolt"
+        case 300...321:
+            return "cloud.drizzle"
+        case 500...531:
+            return "cloud.rain"
+        case 600...622:
+            return "cloud.snow"
+        case 701...781:
+            return "cloud.fog"
+        case 800:
+            return "sun.max"
+        case 801...804:
+            return "cloud.bolt"
+        default:
+            return "cloud"
+        }
+
     }
 }
